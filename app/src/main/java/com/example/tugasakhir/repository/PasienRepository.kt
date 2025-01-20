7db13d2bb92d1b7759e56e9c9515636fb359cd41
DeniArif <deniarifwibowo82@gmail.com>
2025-01-20 18:53:45 +0700

    Perbaikan kode pada PasienRepository.kt

--- app/src/main/java/com/example/tugasakhir/repository/PasienRepository.kt
+++ app/src/main/java/com/example/tugasakhir/repository/PasienRepository.kt
@@ -1,58 +1,53 @@
-package com.example.tugasakhir.repository
 
-import com.example.tugasakhir.model.AllPasienResponse
 import com.example.tugasakhir.model.Pasien
 import com.example.tugasakhir.service.PasienApiService
-import java.io.IOException
+import okio.IOException
 
+interface PasienRepository{
+    suspend fun  getPasien():List<Pasien>
 
-interface PasienRepository {
-    // Mengambil daftar semua pasien
-    suspend fun getAllPasien(): AllPasienResponse
-
-    // Menambahkan data pasien
     suspend fun insertPasien(pasien: Pasien)
 
-    // Memperbarui data pasien berdasarkan ID
-    suspend fun updatePasien(idPasien: Int, pasien: Pasien)
+    suspend fun updatePasien(idpasien: String,pasien: Pasien)
 
-    // Menghapus pasien berdasarkan ID
-    suspend fun deletePasien(idPasien: Int)
+    suspend fun deletePasien(idpasien: String)
 
-    // Mengambil data pasien berdasarkan ID
-    suspend fun getPasienById(idPasien: Int): Pasien
+    suspend fun getPasienByid(idpasien: String):Pasien
 }
 
-class NetworkPasienRepository(
+class NetworkMahasiswaRepository(
     private val pasienApiService: PasienApiService
 ) : PasienRepository {
-    override suspend fun getAllPasien(): AllPasienResponse =
+    override suspend fun getPasien(): List<Pasien> =
         pasienApiService.getAllPasien()
 
     override suspend fun insertPasien(pasien: Pasien) {
         pasienApiService.insertPasien(pasien)
     }
 
-    override suspend fun updatePasien(idPasien: Int, pasien: Pasien) {
-        pasienApiService.updatePasien(idPasien, pasien)
+    override suspend fun updatePasien(idpasien: String, pasien: Pasien) {
+        pasienApiService.updatePasien(idpasien, pasien)
     }
 
-    override suspend fun deletePasien(idPasien: Int) {
+    override suspend fun deletePasien(idPasien: String) {
         try {
             val response = pasienApiService.deletePasien(idPasien)
             if (!response.isSuccessful) {
                 throw IOException(
                     "Failed to delete Pasien. HTTP Status Code: ${response.code()}"
                 )
-            } else {
+            }else   {
                 println(response.message())
             }
-        } catch (e: Exception) {
+        }catch (e: Exception){
             throw e
         }
     }
 
-    override suspend fun getPasienById(idPasien: Int): Pasien {
-        return pasienApiService.getPasienById(idPasien).data
+    override suspend fun getPasienByid(idpasien: String): Pasien {
+        return pasienApiService.getPasienById(idpasien)
     }
+
+
+
 }
