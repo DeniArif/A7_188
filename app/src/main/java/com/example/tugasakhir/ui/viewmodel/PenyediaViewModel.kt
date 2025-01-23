d05ba5c880c30f1660a399314c2cb3fe9c85811c
DeniArif <deniarifwibowo82@gmail.com>
2025-01-23 20:56:55 +0700

    Membuat kode di PenyediaViewModel.kt untuk menyediakan ViewModel dengan dependensi yang diperlukan

--- app/src/main/java/com/example/tugasakhir/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugasakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.tugasakhir.ui.viewmodel
 
+
+
+
+import DetailPasienViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.tugasakhir.RumahSakitApplication
+import com.example.tugasakhir.ui.viewmodel.pasien.HomePasienViewModel
+import com.example.tugasakhir.ui.viewmodel.pasien.InsertPasienViewModel
+import com.example.tugasakhir.ui.viewmodel.pasien.UpdatePasienViewModel
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomePasienViewModel(DaftarPasien().container.pasienRepository)}
+        initializer { DetailPasienViewModel(createSavedStateHandle(),RumahSakitApplication().container.pasienRepository) }
+        initializer { InsertPasienViewModel(DaftarPasien().container.pasienRepository) }
+        initializer { UpdatePasienViewModel(createSavedStateHandle(), RumahSakitApplication().container.pasienRepository) }
+
+    }
+
+    fun CreationExtras.DaftarPasien(): RumahSakitApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RumahSakitApplication)
+}
