e07ff24b10df46ee72f64dc11de9ea20bef07321
DeniArif <deniarifwibowo82@gmail.com>
2025-01-25 01:52:25 +0700

    Perbaikan HomePasienViewModel.kt

--- app/src/main/java/com/example/tugasakhir/ui/viewmodel/pasien/HomePasienViewModel.kt
+++ app/src/main/java/com/example/tugasakhir/ui/viewmodel/pasien/HomePasienViewModel.kt
@@ -1,53 +1,52 @@
 package com.example.tugasakhir.ui.viewmodel.pasien
 
-
-
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import coil.network.HttpException
 import com.example.tugasakhir.model.Pasien
 import com.example.tugasakhir.repository.PasienRepository
 import kotlinx.coroutines.launch
-import okio.IOException
+import java.io.IOException
 
-sealed class HomePasienUiState {
-    data class Success(val pasien: List<Pasien>) : HomePasienUiState()
-    object Error : HomePasienUiState()
-    object Loading : HomePasienUiState()
+
+sealed class HomePsnUiState {
+    data class Success(val pasien: List<Pasien>) : HomePsnUiState()
+    object Error : HomePsnUiState()
+    object Loading : HomePsnUiState()
 }
 
-class HomePasienViewModel(private val psn: PasienRepository) : ViewModel() {
-    var pasienUiState: HomePasienUiState by mutableStateOf(HomePasienUiState.Loading)
+class HomePasienViewModel(private val pasienRepository: PasienRepository): ViewModel(){
+    var psnUiState:HomePsnUiState by mutableStateOf(HomePsnUiState.Loading)
         private set
 
     init {
-        getPasien()
+        getPsn()
     }
 
-    fun getPasien() {
+    fun getPsn() {
         viewModelScope.launch {
-            pasienUiState = HomePasienUiState.Loading
-            pasienUiState = try {
-                HomePasienUiState.Success(psn.getAllPasien().data)
-            } catch (e: IOException) {
-                HomePasienUiState.Error
-            } catch (e: HttpException) {
-                HomePasienUiState.Error
+            psnUiState = HomePsnUiState.Loading
+            psnUiState = try {
+                val response = pasienRepository.getAllPasien()
+                HomePsnUiState.Success(response.data)
+            } catch ( e: IOException) {
+                HomePsnUiState.Error
+            } catch (e:IOException) {
+                HomePsnUiState.Error
             }
         }
     }
 
-    fun deletePasien(idPasien: String) {
+    fun deletePsn(id_pasien : String){
         viewModelScope.launch {
             try {
-                psn.deletePasien(idPasien)
-            } catch (e: IOException) {
-                HomePasienUiState.Error
-            } catch (e: HttpException) {
-                HomePasienUiState.Error
+                pasienRepository.deletePasien(id_pasien)
+            } catch (e: IOException){
+                psnUiState = HomePsnUiState.Error
+            } catch (e: IOException){
+                psnUiState = HomePsnUiState.Error
             }
         }
     }
