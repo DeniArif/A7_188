2f76b8a12f5afe49327c9568d5b8969c39947afd
DeniArif <deniarifwibowo82@gmail.com>
2025-01-25 15:48:17 +0700

    Perbaikan di kode PenyediaViewModel.kt

--- app/src/main/java/com/example/tugasakhir/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugasakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -3,24 +3,26 @@ package com.example.tugasakhir.ui.viewmodel
 
 
 
-import DetailPasienViewModel
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.tugasakhir.RumahSakitApplication
+import com.example.tugasakhir.ui.viewmodel.pasien.DetailPasienViewModel
 import com.example.tugasakhir.ui.viewmodel.pasien.HomePasienViewModel
-import com.example.tugasakhir.ui.viewmodel.pasien.InsertPasienViewModel
-import com.example.tugasakhir.ui.viewmodel.pasien.UpdatePasienViewModel
+import com.example.tugasakhir.ui.viewmodel.pasien.InsertPsnViewModel
+
+//import com.example.tugasakhir.ui.viewmodel.pasien.UpdatePasienViewModel
 
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomePasienViewModel(DaftarPasien().container.pasienRepository)}
         initializer { DetailPasienViewModel(createSavedStateHandle(),RumahSakitApplication().container.pasienRepository) }
-        initializer { InsertPasienViewModel(DaftarPasien().container.pasienRepository) }
-        initializer { UpdatePasienViewModel(createSavedStateHandle(), RumahSakitApplication().container.pasienRepository) }
+        initializer { InsertPsnViewModel(DaftarPasien().container.pasienRepository) }
+        //initializer { UpdatePasienViewModel(createSavedStateHandle(), RumahSakitApplication().container.pasienRepository) }
 
     }
 
