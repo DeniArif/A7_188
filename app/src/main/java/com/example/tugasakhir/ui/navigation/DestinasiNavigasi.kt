b7977844eaddd0d5437ed8892722d94480e208cc
DeniArif <deniarifwibowo82@gmail.com>
2025-01-26 21:24:12 +0700

    Perbaikan di kode DestinasiNavigasi.kt

--- app/src/main/java/com/example/tugasakhir/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/tugasakhir/ui/navigation/DestinasiNavigasi.kt
@@ -4,3 +4,13 @@ interface DestinasiNavigasi{
     val route : String
     val titleRes : String
 }
+
+object HomeDestinasi : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
+object DetailDestinasi : DestinasiNavigasi {
+    override val route: String = "detail/{id}"
+    override val titleRes: String = "Detail"
+}
