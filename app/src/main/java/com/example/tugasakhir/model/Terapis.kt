7535298929d7a717575a2a878ea3714770004d38
DeniArif <deniarifwibowo82@gmail.com>
2025-01-28 01:03:56 +0700

    Membuat Kode isi model Terapis.kt

---
+++ app/src/main/java/com/example/tugasakhir/model/Terapis.kt
@@ -0,0 +1,23 @@
+package com.example.tugasakhir.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Terapis(
+    val id_terapis : Int,
+    val nama_terapis: String,
+    val spesialisasi: String,
+    val nomor_izin_praktik: String
+)
+@Serializable
+data class AllTerapisResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Terapis>
+)
+@Serializable
+data class TerapisDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Terapis
+)
