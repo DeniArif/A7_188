f8b5f43c161182682e543d6a27d33b0781607bee
DeniArif <deniarifwibowo82@gmail.com>
2025-01-28 21:09:31 +0700

    Perbaikan kode UpdateTerapisView.kt

--- app/src/main/java/com/example/tugasakhir/ui/view/terapis/UpdateTerapisView.kt
+++ app/src/main/java/com/example/tugasakhir/ui/view/terapis/UpdateTerapisView.kt
@@ -2,26 +2,33 @@ package com.example.tugasakhir.ui.view.terapis
 
 import CostumeTopAppBar
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tugasakhir.ui.navigation.DestinasiNavigasi
+import com.example.tugasakhir.ui.view.pasien.DestinasiUpdatePsn
 import com.example.tugasakhir.ui.viewmodel.PenyediaViewModel
 import com.example.tugasakhir.ui.viewmodel.terapis.UpdateTrpViewModel
-import com.example.tugasakhir.ui.viewmodel.terapis.toTerapis
 import kotlinx.coroutines.launch
 
 
 object DestinasiUpdateTrp : DestinasiNavigasi {
-    override val route = "update_jenis"
+    override val route = "update"
     const val ID_TERAPIS = "id_terapis"
     val routesWithArg = "$route/{$ID_TERAPIS}"
     override val titleRes = "Update Terapis"
@@ -30,22 +37,32 @@ object DestinasiUpdateTrp : DestinasiNavigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TerapisUpdateView(
-    id_terapis: Int,
+    id_terapis: String,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: UpdateTrpViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val nama_terapis by viewModel.nama_terapis
+    val spesialisasi by viewModel.spesialisasi
+    val nomor_izin_praktik by viewModel.nomor_izin_praktik
+
+
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-    // Collect the UI state from the ViewModel
+
     val uiState = viewModel.TrpuiState.value
+
+
+    if (id_terapis.isNotEmpty()) {
+        viewModel.loadTrpData()  // Memastikan data pasien dimuat
+    }
 
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             CostumeTopAppBar(
-                title = DestinasiUpdateTrp.titleRes,
+                title = DestinasiUpdatePsn.titleRes,
                 canNavigateBack = true,
                 scrollBehavior = scrollBehavior,
                 navigateUp = navigateBack
@@ -58,25 +75,47 @@ fun TerapisUpdateView(
                 .padding(padding)
                 .padding(16.dp)
         ) {
-            // Pass the UI state to EntryBody to handle form input
-            EntryBody(
-                insertTrpUiState = uiState, // Sebelumnya salah referensi
-                onValueChange = { updatedValue ->
-                    viewModel.updateTrpState(updatedValue) // Update ViewModel state with new data
-                },
-                onSaveClick = {
-                    uiState.insertTrpUiEvent?.let { insertUiEventTrp -> // Sebelumnya salah penulisan
-                        coroutineScope.launch {
-                            // Call ViewModel update method with updated Pasien
-                            viewModel.updateTrp(
-                                id_terapis = viewModel.id_terapis, // Pass the patient ID from ViewModel
-                                terapis = insertUiEventTrp.toTerapis() // Convert InsertUiEvent to Pasien
-                            )
-                            navigateBack() // Navigate back after saving changes
-                        }
-                    }
-                }
+            OutlinedTextField(
+                value = nama_terapis,
+                onValueChange = {viewModel.nama_terapis.value = it},
+                label = { Text("Nama Terapis") },
+                modifier = Modifier.fillMaxWidth()
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = spesialisasi,
+                onValueChange = {viewModel.spesialisasi.value = it},
+                label = { Text("Spesialisasi") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = nomor_izin_praktik,
+                onValueChange = {viewModel.nomor_izin_praktik.value = it},
+                label = { Text("Nomor Telepon") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            // Pass the UI state to EntryBody to handle form input
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.updateTrp()
+                        navigateBack()
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Simpan Perubahan")
+            }
+
         }
     }
 }
