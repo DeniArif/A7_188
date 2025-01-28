50233bbe1ddb64705437b70931c24b4de244c93e
DeniArif <deniarifwibowo82@gmail.com>
2025-01-28 21:10:40 +0700

    Memanggil Setiap viewmodel ke PenyediaViewModel.kt

--- app/src/main/java/com/example/tugasakhir/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugasakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -10,19 +10,37 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.tugasakhir.RumahSakitApplication
+import com.example.tugasakhir.ui.viewmodel.jenisterapis.DetailJenisTerapiViewModel
+import com.example.tugasakhir.ui.viewmodel.jenisterapis.HomeJenisTerapiViewModel
+import com.example.tugasakhir.ui.viewmodel.jenisterapis.InsertJenisTerapiViewModel
+import com.example.tugasakhir.ui.viewmodel.jenisterapis.UpdateJenisTerapiViewModel
 import com.example.tugasakhir.ui.viewmodel.pasien.DetailPasienViewModel
 import com.example.tugasakhir.ui.viewmodel.pasien.HomePasienViewModel
 import com.example.tugasakhir.ui.viewmodel.pasien.InsertPsnViewModel
-
-//import com.example.tugasakhir.ui.viewmodel.pasien.UpdatePasienViewModel
+import com.example.tugasakhir.ui.viewmodel.pasien.UpdatePsnViewModel
+import com.example.tugasakhir.ui.viewmodel.terapis.DetailTerapisViewModel
+import com.example.tugasakhir.ui.viewmodel.terapis.HomeTerapisViewModel
+import com.example.tugasakhir.ui.viewmodel.terapis.InsertTrpViewModel
+import com.example.tugasakhir.ui.viewmodel.terapis.UpdateTrpViewModel
 
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomePasienViewModel(DaftarPasien().container.pasienRepository)}
-        initializer { DetailPasienViewModel(createSavedStateHandle(),RumahSakitApplication().container.pasienRepository) }
+        initializer { DetailPasienViewModel(createSavedStateHandle(),DaftarPasien().container.pasienRepository) }
         initializer { InsertPsnViewModel(DaftarPasien().container.pasienRepository) }
-        //initializer { UpdatePasienViewModel(createSavedStateHandle(), RumahSakitApplication().container.pasienRepository) }
+        initializer { UpdatePsnViewModel(createSavedStateHandle(),DaftarPasien() .container.pasienRepository) }
+
+
+        initializer { HomeTerapisViewModel(DaftarPasien().container.terapisRepository) }
+        initializer { DetailTerapisViewModel(createSavedStateHandle(),DaftarPasien().container.terapisRepository) }
+        initializer { InsertTrpViewModel(DaftarPasien().container.terapisRepository) }
+        initializer { UpdateTrpViewModel(createSavedStateHandle(),DaftarPasien() .container.terapisRepository) }
+
+        initializer { HomeJenisTerapiViewModel(DaftarPasien().container.jenisRepository) }
+        initializer { InsertJenisTerapiViewModel(DaftarPasien().container.jenisRepository) }
+        initializer { UpdateJenisTerapiViewModel(createSavedStateHandle(),DaftarPasien().container.jenisRepository) }
+        initializer { DetailJenisTerapiViewModel(createSavedStateHandle(),DaftarPasien().container.jenisRepository) }
 
     }
 
